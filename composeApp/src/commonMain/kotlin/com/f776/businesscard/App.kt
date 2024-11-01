cebdd621beacff0f246621d7f2a2fd805d29f04e
f776 <fern.belmar@duocuc.cl>
2024-11-01 00:03:57 -0300

    progress

--- composeApp/src/commonMain/kotlin/com/f776/businesscard/App.kt
+++ composeApp/src/commonMain/kotlin/com/f776/businesscard/App.kt
@@ -1,37 +1,161 @@
 package com.f776.businesscard
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Call
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import businesscard.composeapp.generated.resources.Res
+import businesscard.composeapp.generated.resources.android_logo
+import businesscard.composeapp.generated.resources.email
+import businesscard.composeapp.generated.resources.name
+import businesscard.composeapp.generated.resources.phone_number
+import businesscard.composeapp.generated.resources.social_media_handle
+import businesscard.composeapp.generated.resources.title
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import businesscard.composeapp.generated.resources.Res
-import businesscard.composeapp.generated.resources.compose_multiplatform
+
+val textColor = Color(0xFFC1C5CF)
+val secondaryTextColor = Color(0xFF01E8C6)
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        val bgColor = Color(0xFF181C28)
+
+        Box(
+            modifier = Modifier
+                .background(bgColor)
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                BusinessCard()
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            ContactInfo(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 32.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun BusinessCard(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.android_logo),
+            contentDescription = null,
+            modifier = Modifier.size(144.dp)
+        )
+        Text(
+            text = stringResource(Res.string.name),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 32.sp,
+            color = textColor
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = stringResource(Res.string.title),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            color = secondaryTextColor
+        )
+    }
+}
+
+@Composable
+fun ContactInfo(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Call,
+                contentDescription = "telephone",
+                tint = secondaryTextColor,
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = stringResource(Res.string.phone_number),
+                color = textColor,
+                textAlign = TextAlign.Start
+            )
+        }
+        Spacer(Modifier.height(8.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Add,
+                contentDescription = "media handle",
+                tint = secondaryTextColor
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = stringResource(Res.string.social_media_handle),
+                color = textColor,
+                textAlign = TextAlign.Start
+            )
+        }
+        Spacer(Modifier.height(8.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.MailOutline,
+                contentDescription = "mail",
+                tint = secondaryTextColor
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = stringResource(Res.string.email),
+                color = textColor,
+                textAlign = TextAlign.Start
+            )
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/f776/businesscard/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.f776.businesscard
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
